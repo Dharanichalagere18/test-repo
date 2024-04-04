a8052e0ce9294e6b35a585bed096b58b659c30f6
Dharanichalagere18 <chalageredharani@gmail.com>
2024-04-04 22:22:08 +0530

    branch2commit3

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Hello World master!!</h1>
         <h1>Hello World Branch2</h1>
         <h1>Hello World Branch2 commit2</h1>
-        <h1>Hello World Branch2</h1>
+        <h1>Hello World </h1>
     </div>
   )
 }
